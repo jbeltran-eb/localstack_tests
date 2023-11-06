e34d46d658e4d3a59156044e7d1d7e1ec2b46862
Jose Maria Beltran Vargas <jbeltran@jbeltran@eventbrite.com-36413>
2023-11-06 12:22:46 +0100

    Removing non required imports

--- aws_hunters_integration_cdk/lib/cloudtrail_tlz_logging_stack.ts
+++ aws_hunters_integration_cdk/lib/cloudtrail_tlz_logging_stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
-import * as snsSubscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3Notifications from 'aws-cdk-lib/aws-s3-notifications';
 import { Construct } from 'constructs';
